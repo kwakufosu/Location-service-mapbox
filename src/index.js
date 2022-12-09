f8c3cd4b0c0355f42852ff3ec9fc7368e4722efd
kwaku fosu <kwakuboachiefosu@gmail.com>
2022-12-09 12:34:51 +0000

    split destination and source geocoding

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import axios from 'axios';
-import { geocode } from './utils/geocoding.js';
+import { srcGeocode,destGeocode} from './utils/geocoding.js';
 
 const app = express();
 const port = process.env.port || 3000;
 
-const srcLongitude = -0.09019875;
-const srcLatitude = 5.628826512;
-const destLongitude = -0.182289882;
-const destLatitude = 5.677741181;
+const srcMeridian = await srcGeocode('spintex');
+
+const [srcLongitude, srcLatitude] = srcMeridian[0].geometry.coordinates;
+
+
+// const destMeridian = await meridians.result.filter(
+//   (meridian) => meridian.text.toLowerCase() === meridians.dest
+// );
+// console.log(destMeridian);
+// const [destLongitude, destLatitude] = destMeridian[0].geometry.coordinates;
 
 const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${srcLongitude},${srcLatitude};${destLongitude},${destLatitude}?alternatives=false&exclude=ferry&geometries=geojson&language=en&overview=simplified&steps=true&access_token=${process.env.MAPBOX_TOKEN}`;
 
@@ -22,10 +28,7 @@ const fetchData = async () => {
     throw new Error(e);
   }
 };
-const meridians = await geocode('Spintex ', 'Tema ');
-const srcMeridian= await meridians.filter((meridian)=> meridian.text=='Spintex')
-const destMeridian= await meridians.filter((meridian)=> meridian.text=='Tema')
-console.log(destMeridian[0].geometry.coordinates)
+
 //fetchData();
 
 app.listen(port, () => {
--- src/utils/geocoding.js
+++ src/utils/geocoding.js
@@ -6,18 +6,40 @@ dotenv.config();
 const baseClient = mbxClient({ accessToken: process.env.MAPBOX_TOKEN });
 const geocodingService = geocodingClient(baseClient);
 
-export const geocode = async (source, dest) => {
+export const srcGeocode = async (source) => {
+  source = source.trim().toLowerCase();
+  
   const result = await geocodingService
     .forwardGeocode({
-      query: `${source}, ${dest}`,
+      query: `${source}`,
       countries: ['GH'],
-      limit: 2,
+      limit: 1,
     })
     .send()
     .then((response) => {
       const match = response.body;
+      console.log(match);
       return match.features;
     });
 
   return result;
 };
+
+export const destGeocode = async (dest) => {
+  dest= dest.trim().toLowerCase();
+  
+  const result = await geocodingService
+    .forwardGeocode({
+      query: `${source}`,
+      countries: ['GH'],
+      limit: 1,
+    })
+    .send()
+    .then((response) => {
+      const match = response.body;
+       return match.features;
+    });
+
+  return { result, dest };
+};
+
